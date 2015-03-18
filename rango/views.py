46eed7b14734ca2036c1fd2159da3cb8eb5bf029
Blake Carpenter <wbcarpenter066@gmail.com>
2015-03-18 01:51:00 -0400

    Finished forms, user profile storage, starting login system

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
-from rango.forms import CategoryForm, PageForm
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 def index(request):
     cat = Category.objects.order_by('-likes')[:5]
@@ -40,16 +40,58 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form':form})
 
-def add_page(request):
+def add_page(request, category_name_slug):
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+            cat = None
+
     if request.method == 'POST':
         form = PageForm(request.POST)
 
         if form.is_valid():
-            form.save(commit=True)
-            return index(request)
+            page = form.save(commit=False)
+            page.category = cat
+            page.views = 0
+            page.save()
+            return category(request, category_name_slug)
         else:
             print form.errors
     else:
         form = PageForm()
 
-    return render(request, 'rango/add_page.html', {'form':form})
+    context_dict = {'form':form,'category':cat}
+    return render(request, 'rango/add_page.html', context_dict)
+
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+
+            registered = True
+
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+            'rango/register.html',
+            {'user_form':user_form, 'profile_form':profile_form,'registered':registered})
